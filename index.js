38d4f5e1138b4ab30a36de6894f0ffad97d7288f
綾小路 ろいと <ayanokoji.roito@gmail.com>
2024-11-16 13:12:57 +0900

    commit

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     
-    // デモ用の認証情報
     if (email === "demo@example.com" && password === "password123") {
-        // 認証成功後、home.htmlへ遷移
         window.location.href = "home.html";
     } else {
-        // 認証失敗時のエラーメッセージ
-        alert("無効です。");
+        alert("無効な資格情報です");
     }
 }
